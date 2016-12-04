6d6f9827baad7ddef714896d81b1287cac29cd40
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-03 21:54:41 -0500

    method

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -2,4 +2,21 @@ package day.one;
 
 public class MethodPractice {
 
+	// Write a method that your name input and return your name.
+
+	public static String getNewName(String name) {
+
+		String myName = null;
+
+		myName = name;
+
+		return myName;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(getNewName("ibrahim"));
+
+	}
+
 }
