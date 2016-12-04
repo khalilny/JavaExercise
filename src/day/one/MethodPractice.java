a247ea1b8bd01c36ccfe0c30a2184c4f394eda55
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-03 22:41:05 -0500

    Second Method

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -1,5 +1,7 @@
 package day.one;
 
+import java.util.ArrayList;
+
 public class MethodPractice {
 
 	// Write a method that your name input and return your name.
@@ -12,10 +14,45 @@ public class MethodPractice {
 
 		return myName;
 	}
+	
+	
+	// Write a method that take one number input and return the list of all even number 
+	
+	
+	public static ArrayList<Integer>getEvenNumberList(int number){
+		ArrayList<Integer> evenNumber = new ArrayList<Integer>();
+		
+		for(int i= 0; i<=number;i++){
+			
+			
+			if(i%2==0){
+				
+				
+				evenNumber.add(i);
+				
+				
+				
+			}
+			
+		}
+		
+		
+		
+		return evenNumber;
+		
+	}
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 
 		System.out.println(getNewName("ibrahim"));
+		
+		System.out.println(getEvenNumberList(20));
+		
 
 	}
 
