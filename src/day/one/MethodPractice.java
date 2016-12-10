d5657d7e05bb09e5ed6af8b61c10963785c7cf58
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-09 19:14:54 -0500

    Threesimple  Method Solutin

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class MethodPractice {
 
-	// Write a method that your name input and return your name.
+	// 1. Write a method that take your name input and return your name.
 
 	public static String getNewName(String name) {
 
@@ -14,45 +14,51 @@ public class MethodPractice {
 
 		return myName;
 	}
-	
-	
-	// Write a method that take one number input and return the list of all even number 
-	
-	
-	public static ArrayList<Integer>getEvenNumberList(int number){
+
+	// 2. Write a method that take one number input and return the list of all even number
+
+	public static ArrayList<Integer> getEvenNumberList(int number) {
 		ArrayList<Integer> evenNumber = new ArrayList<Integer>();
-		
-		for(int i= 0; i<=number;i++){
-			
-			
-			if(i%2==0){
-				
-				
+
+		for (int i = 0; i <= number; i++) {
+
+			if (i % 2 == 0) {
+
 				evenNumber.add(i);
-				
-				
-				
+
 			}
-			
+
 		}
-		
-		
-		
+
 		return evenNumber;
-		
+
 	}
-	
-	
-	
-	
-	
+
+	// 3. Write a method that take a number input and return the list of
+	// all odd number from 0 to that given number.
+
+	public static ArrayList<Integer> getOddNumberList(int number) {
+
+		ArrayList<Integer> oddNumberList = new ArrayList<Integer>();
+
+		for (int i = 0; i <= number; i++) {
+
+			if (i % 2 == 1) {
+
+				oddNumberList.add(i);
+			}
+		}
+
+		return oddNumberList;
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println(getNewName("ibrahim"));
-		
-		System.out.println(getEvenNumberList(20));
-		
+		System.out.println("(Ans.-1.)" + getNewName("ibrahim"));
+
+		System.out.println("(Ans.-2.)" + getEvenNumberList(20));
+
+		System.out.println("(Ans.-3.)" + getOddNumberList(20));
 
 	}
 
