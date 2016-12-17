2fbacd48cf8a8d0c78596c5e2df2a95756e4c217
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-17 10:20:29 -0500

    Please add method # 7 for index find outt

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -1,5 +1,6 @@
 package day.one;
 
+import java.util.ArrayList;
 
 public class MethodPractice {
 
@@ -13,9 +14,23 @@ public class MethodPractice {
 		return upperCase;
 	}
 
+	// 7. Write a method that take one String input and return a list off all index.
+
+	public static ArrayList<Integer> getIndex(String text) {
+		ArrayList<Integer> allIndex = new ArrayList<Integer>();
+
+		for (int i = 0; i <= text.length(); i++) {
+
+			allIndex.add(i);
+		}
+
+		return allIndex;
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("(Ans.-5.)" + getStringUpperCase("This is a String"));
+		System.out.println(getIndex("This is a String"));
 
 	}
 
