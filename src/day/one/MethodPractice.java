8c8281a395503b2666ec901a76a0e9313857bb50
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-22 19:27:00 -0500

    Add new method exercise # 9

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -8,9 +8,7 @@ public class MethodPractice {
 
 	public static String getStringUpperCase(String text) {
 		String upperCase = null;
-
 		upperCase = text.toUpperCase();
-
 		return upperCase;
 	}
 
@@ -20,34 +18,38 @@ public class MethodPractice {
 		ArrayList<Integer> allIndex = new ArrayList<Integer>();
 
 		for (int i = 0; i <= text.length(); i++) {
-
 			allIndex.add(i);
 		}
-
 		return allIndex;
 	}
-	
+
 	// 8. Write a method that take one String input and return a list of all character
-	
-	public static ArrayList<Character> getCharacterList(String text){
+
+	public static ArrayList<Character> getCharacterList(String text) {
 		ArrayList<Character> allCharacter = new ArrayList<Character>();
-		
-		for(int i =0; i<text.length();i++){
-			
-			
+
+		for (int i = 0; i < text.length(); i++) {
 			allCharacter.add(text.charAt(i));
 		}
-		
 		return allCharacter;
 	}
-	
-	
+
+	// 9.Write a method that take one number input and return the sum of 1 to that given number.
+
+	public static int getSum(int number) {
+		int total = 0;
+		for (int i = 0; i <= number; i++) {
+			total = total + i;
+		}
+		return total;
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("(Ans.-5.)" + getStringUpperCase("This is a String"));
-		System.out.println("Ans.-7.)"+ getIndex("This is a String"));
-		System.out.println("Ans.-8.)"+ getCharacterList("This is a String"));
+		System.out.println("Ans.-7.)" + getIndex("This is a String"));
+		System.out.println("Ans.-8.)" + getCharacterList("This is a String"));
+		System.out.println("Ans.-9.) " + getSum(20));
 	}
 
 }
