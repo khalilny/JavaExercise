3ccb123d8040f456c9944f5fed3c4c60702aa533
Muhammad Khalil <muhammadkhalil@Muhammads-MacBook-Pro.local>
2016-12-26 22:19:14 -0500

    Add Method Exercise # 1 to 13, update

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -3,24 +3,97 @@ package day.one;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
+
 public class MethodPractice {
+
+	/**
+	 * 1. Write a method that take your name input and return your name.
+	 * Input/Parameter: string name return/output : String Serving
+	 * Bucket/container:variable
+	 */
+
+	public static String getNewNameOne(String name) {
+		String newName = "";
+		newName = name;
+
+		return newName;
+	}
+
+	/**
+	 * 2. Write a method that take a number input and return the list of all
+	 * even number from 0 to that given number. 
+	 * Input/ Parameter: int number
+	 * return/output type : ArrayList <Integer>
+	 *  Serving Bucket/container: List
+	 */
+
+	public static ArrayList<Integer> getEvenNumber(int number) {
+		ArrayList<Integer> evenNumberList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i = i + 2) {
+			evenNumberList.add(i);
+		}
+
+		return evenNumberList;
+	}
+
+	/**
+	 * 3. Write a method that take a number input and return the list of all odd
+	 * number from 0 to that given number
+	 * Input/ parameter int number
+	 * return/output type: ArrayList<Integer>
+	 *  serving bucket/ container List.
+	 */
+	public static ArrayList<Integer> getOddNumber(int number) {
+		ArrayList<Integer> oddNumber = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i++) {
+			if (i % 2 == 1) {
+				oddNumber.add(i);
+			}
+		}
+
+		return oddNumber;
+	}
+
+	/**
+	 * 4.Write a method that take input as mile & return as kilometer.
+	 *  Input/ Parameter: double number
+	 *   return type:double
+	 *    serving Bucket/container:variable
+	 */
+	public static double getKilometer(double number) {
+		double asKilometer = 0;
+		asKilometer = number * 1.6;
+
+		return asKilometer;
+	}
 
 	// 5. Write a Method that take one String input and return same string in upper case.
 
 	public static String getStringUpperCase(String text) {
 		String upperCase = null;
 		upperCase = text.toUpperCase();
+
 		return upperCase;
+	}
+
+	// Write a method that take one String input and return Same String in lower case.
+
+	public static String getStringLowerCase(String text) {
+		String lowerCase = null;
+		lowerCase = text.toLowerCase();
+
+		return lowerCase;
 	}
 
 	// 7. Write a method that take one String input and return a list off all index.
 
 	public static ArrayList<Integer> getIndex(String text) {
 		ArrayList<Integer> allIndex = new ArrayList<Integer>();
-
 		for (int i = 0; i <= text.length(); i++) {
 			allIndex.add(i);
 		}
+
 		return allIndex;
 	}
 
@@ -44,43 +117,69 @@ public class MethodPractice {
 		}
 		return total;
 	}
-//10 Write a method that take one number input and return square of the given number
-	
-	public static int getSquareNumber( int number){
+	// 10 Write a method that take one number input and return square of the given number
+
+	public static int getSquareNumber(int number) {
 		int squareTotal = 0;
-		squareTotal = number*number;
+		squareTotal = number * number;
 		return squareTotal;
 	}
-	
-	// 11. Write a method taht take one number input and return an array that contain 0 to given number
-	
-	public static int[] getNumberArray(int number){
-		int[]numArray = new int[number+1];
-		for(int i=0; i<=number; i++){
-			numArray[i]=i;
+
+	// 11. Write a method taht take one number input and return an array that
+	// contain 0 to given number
+
+	public static int[] getNumberArray(int number) {
+		int[] numArray = new int[number + 1];
+		for (int i = 0; i <= number; i++) {
+			numArray[i] = i;
 		}
 		return numArray;
 	}
-	
-	//12. Write a method that take one number input and return a number list starting from 0 to that given number
-	
-	public static ArrayList<Integer> getNumberList(int number){
-		ArrayList<Integer>numberList = new ArrayList<Integer>();
-		for( int i= 0; i<=number; i++){
+
+	// 12. Write a method that take one number input and return a number list
+	// starting from 0 to that given number
+
+	public static ArrayList<Integer> getNumberList(int number) {
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i++) {
 			numberList.add(i);
 		}
 		return numberList;
 	}
-	
+
+	// 13.Write a method that take one number input and return fizz if the given
+	// number divided by 5 and return buzz if the given number divided by 5 && 3
+	// otherwise return fizz buzz.
+
+	public static String getName(int number) {
+		String result = null;
+		if (number % 5 == 0 && number % 3 == 0) {
+			result = "Buzz";
+		} else if (number % 5 == 0) {
+			result = "Fizz";
+		} else {
+			result = "FizzBuzz";
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println("(Ans.-5.)" + getStringUpperCase("This is a String"));
-		System.out.println("Ans.-7.)" + getIndex("This is a String"));
-		System.out.println("Ans.-8.)" + getCharacterList("This is a String"));
-		System.out.println("Ans.-9.) " + getSum(20));
-		System.out.println("Ans.-10.) " + getSquareNumber(20));
-		System.out.println("Ans.-11.) " + Arrays.toString(getNumberArray(10)));
+		System.out.println("Ans.-13.) " + getName(20));
 		System.out.println("Ans.-12.) " + getNumberList(10));
+		System.out.println("Ans.-11.) " + Arrays.toString(getNumberArray(10)));
+		System.out.println("Ans.-10.) " + getSquareNumber(20));
+		System.out.println("Ans.-9.) " + getSum(20));
+		System.out.println("Ans.-8.) " + getCharacterList("This is a String"));
+		System.out.println("Ans.-7.) " + getIndex("This is a String"));
+		System.out.println("Ans.-6.) " + getStringLowerCase("This is a String"));
+		System.out.println("Ans.-5.) " + getStringUpperCase("This is a String"));
+		System.out.println("Ans.-4.) " + getKilometer(3));
+		System.out.println("Ans.-3.) " + getOddNumber(20));
+		System.out.println("Ans.-2.) " + getEvenNumber(20));
+		System.out.println("Ans.-1.) " + getNewNameOne("Rafique"));
+
 	}
 
 }
